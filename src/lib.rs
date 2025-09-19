378de893fe426b4e52c9a343bbfad8ce5cc4b799
Vasco Schiavo <115561717+VascoSch92@users.noreply.github.com>
2025-09-19 14:13:14 +0200

    [EHN] add permutation core (#12)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 mod elements;
 
 use crate::elements::permutation::{
-    ascents, descents, exceedances, int_repr, is_derangement, lehmer_code, lexicographic_rank, repr,
+    ascents, call_on_int, call_on_str, descents, exceedances, int_repr, inversions, is_derangement,
+    lehmer_code, lexicographic_rank, multiplication, records, repr, support,
 };
 use crate::elements::validators::{
     validate_cycle, validate_cycle_decomposition, validate_permutation,
@@ -18,13 +19,19 @@ fn _symmetria_core(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
 
     let permutation = PyModule::new(py, "permutation")?;
     permutation.add_function(wrap_pyfunction!(ascents, py)?)?;
+    permutation.add_function(wrap_pyfunction!(call_on_int, py)?)?;
+    permutation.add_function(wrap_pyfunction!(call_on_str, py)?)?;
     permutation.add_function(wrap_pyfunction!(descents, py)?)?;
     permutation.add_function(wrap_pyfunction!(exceedances, py)?)?;
     permutation.add_function(wrap_pyfunction!(int_repr, py)?)?;
+    permutation.add_function(wrap_pyfunction!(inversions, py)?)?;
     permutation.add_function(wrap_pyfunction!(is_derangement, py)?)?;
     permutation.add_function(wrap_pyfunction!(lehmer_code, py)?)?;
     permutation.add_function(wrap_pyfunction!(lexicographic_rank, py)?)?;
     permutation.add_function(wrap_pyfunction!(repr, py)?)?;
+    permutation.add_function(wrap_pyfunction!(multiplication, py)?)?;
+    permutation.add_function(wrap_pyfunction!(records, py)?)?;
+    permutation.add_function(wrap_pyfunction!(support, py)?)?;
 
     m.add_submodule(&validators)?;
     m.add_submodule(&permutation)?;
